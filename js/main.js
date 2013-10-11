30c0e6c47e1e6367af46cbd293644ac4df823488
meded <meded90@gmail.com>
2013-10-11 21:01:27 +0400

    test api

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ $(function () {
 
 // test
         alert("вроде все гуд")
+
+        VK.api('users.get', function (data) {
+            console.log(data);
+        });
     }, function () {
         alert('жопа че')
     }, '5.2');
