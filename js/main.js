84658db76c792033ab5873d08f8ef16690da8aef
meded <meded90@gmail.com>
2013-10-12 08:31:33 +0400

    test api

--- js/main.js
+++ js/main.js
@@ -1,15 +1,70 @@
 $(function () {
 
+    var Friend = Backbone.Model.extend({
+        default: {
+            id: "",
+            first_name: '',
+            last_name: '',
+            city: '',
+            photo_50: '',
+            photo_100: '',
+            photo_200_orig: '',
+            online: 0,
+            can_write_private_message: 0,
+            mobile_phone: '',
+            home_phone: '',
+            verified: 0,
+            counters: {}
+        },
+        initialize: function () {
+            this.getInfo()
+        },
+        getInfo: function () {
 
+        }
+    });
+    var Friends = Backbone.Collection.extend({
+        model: Friend,
+        initialize: function () {
+            this.getAllFriends();
+        },
+        getAllFriends: function () {
+            var thet = this;
+            VK.api('friends.get', {
+                fields: "can_write_private_message,online,counters,contacts, photo_50, photo_100, photo_200_orig,verified",
+                name_case: "Nom"
+            }, function (data) {
+                _.each(data.response.items, function (item) {
+                    thet.add(item)
+                });
+                thet.trigger("addAllFriends");
+            });
+        }
+    });
+
+    var ViweFriends = Backbone.View.extend({
+        region: $('#list'),
+        template: $('#itemList').html(),
+        initialize: function () {
+            this.friends = new Friends();
+            this.friends.on('addAllFriends', this.startSort(), this)
+        },
+        startSort: function () {
+            this.friends.sortBy(function (item) {
+                debugger;
+            });
+        },
+        render: function () {
+
+        }
+    })
     VK.init(function () {
+        new ViweFriends();
 
-// test
-        alert("вроде все гуд")
-
-        VK.api('users.get', function (data) {
-            console.log(data);
-        });
     }, function () {
         alert('жопа че')
     }, '5.2');
-});
+
+
+})
+;
